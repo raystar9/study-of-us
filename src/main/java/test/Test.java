1d6e56d5fc92d4a4b8a7760c6a72a554a11ba0f0
rumor1993 <rumor1993@naver.com>
2018-05-21 15:14:46 +0900

    서블릿 테스트

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,37 @@
+package test;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Test
+ */
+@WebServlet("/study/search")
+public class Test extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Test() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/*// TODO Auto-generated method stub
+		response.sendRedirect("/study-of-us/study/search/Search_Main.jsp");*/
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/study/search/Search_Main.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
