4546a9700fbed666c56d19fbf0abcb3f1bc097da
Raystar <raystar9@naver.com>
2018-06-03 15:35:33 +0900

    temp

--- src/main/java/fakeDB/FakeDB.java
+++ src/main/java/fakeDB/FakeDB.java
@@ -1,171 +1,105 @@
 package fakeDB;
 
-import java.sql.Date;
 import java.util.ArrayList;
-import java.util.GregorianCalendar;
 
-import beans.prototype.Attendance;
-import beans.prototype.Meeting;
-import beans.prototype.Member;
-import beans.prototype.Study;
-import beans.study.StudyListCount;
-import beans.study.each.AttendanceRecordBean;
-import beans.study.each.schedule.ScheduleBean;
+import beansNew.Attend;
+import beansNew.Board;
+import beansNew.Category;
+import beansNew.FeeCollect;
+import beansNew.FeeSpend;
+import beansNew.Meeting;
+import beansNew.Member;
+import beansNew.Study;
+import dateConverter.DateConverter;
 
 public class FakeDB {
 	private static FakeDB instance = new FakeDB();
-	ArrayList<Member> members = new ArrayList<>();
-	ArrayList<Study> studies1 = new ArrayList<>();
-	ArrayList<StudyListCount> studies = new ArrayList<>();
-	ArrayList<Meeting> meetings = new ArrayList<>();
-	ArrayList<ScheduleBean> schedules = new ArrayList<>();
-	ArrayList<Attendance> attendances = new ArrayList<>();
-	ArrayList<AttendanceRecordBean> records = new ArrayList<>();
+	private ArrayList<Attend> attends = new ArrayList<>();
+	private ArrayList<Board> boards = new ArrayList<>();
+	private ArrayList<Category> categories = new ArrayList<>();
+	private ArrayList<FeeCollect> feeCollects = new ArrayList<>();
+	private ArrayList<FeeSpend> feesSpends = new ArrayList<>();
+	private ArrayList<Meeting> meetings = new ArrayList<>();
+	private ArrayList<Member> members = new ArrayList<>();
+	private ArrayList<Study> studies = new ArrayList<>();
 	
 	public static FakeDB getInstance() {
 		return instance;
 	}
 	
 	private FakeDB() {
-		initMember();
-		initStudy();
+		initMembers();
+		initCategories();
+		initStudies();
+		initBoards();
 		initMeetings();
-		initSchedules();
-		initAttendanceRecord();
-	}
-
-	private void initAttendanceRecord() {
-		AttendanceRecordBean a = new AttendanceRecordBean();
-		//TODO 여기서 작업마침 해야돼!!
-	}
-
-	public void setAttendanceRecord() {
-		
-	}
-	public ArrayList<AttendanceRecordBean> getAttendanceRecords() {
-		return records;
+		initAttends();
+		initFeeCollects();
+		initFeeSpends();
 	}
 	
-	public ArrayList<Attendance> getAttendances() {
-		return attendances;
+	private void initMembers() {
+		for(int i = 0; i < 50; i++) {
+			Member member = new Member();
+			member.setAddress("서울");
+			member.setEmail("a@naver.com");
+			member.setGender("m");
+			member.setId("user" + i);
+			member.setIntroduce("반갑습니다");
+			member.setName("나회원");
+			member.setPassword("1234");
+			member.setStudies(new ArrayList<Study>());
+			member.setTel("0100000000");
+		}
+		
 	}
 
-	public void addAttendances(Attendance attendance) {
-		attendances.add(attendance);
+	private void initStudies() {
+		for(int i = 0; i < 30; i++) {
+			Study study = new Study();
+			study.setCategory(new Category("프로그래밍","자바"));
+			study.setDayOfWeek("금");
+			study.setEffects("자바왕이 됩니다.");
+			study.setEndDate(DateConverter.convertDate("2018-05-03"));
+			study.setExplain("어서오세요!");
+			study.setMaterial("준비물 없음");
+			study.setMaxMember(6);
+			study.setMembers(new ArrayList<>());
+			study.setName("자바스터디왕");
+			study.setPlace("서울");
+			study.setStartDate(DateConverter.convertDate("2018-04-07"));
+			study.setTime("7시");
+		}
 	}
 
-	private void initSchedules() {
-		ScheduleBean s1 = new ScheduleBean();
-		s1.setStart("2018-05-03T12:00:00+09:00");
-		s1.setTitle("안녕!");
-		//TODO 수정해야될 예정...
-		s1.setUrl("/study-of-us/study/each/schedule/each");
-		schedules.add(s1);
-	}
 
 	private void initMeetings() {
-		Meeting meeting1 = new Meeting();
-		meeting1.setComment("내용내용");
-		meeting1.setDate(new Date(new GregorianCalendar(2018, 4, 5, 22, 00).getTimeInMillis()));
-		meeting1.setFee(5000);
-		meeting1.setLocation("종각역");
-		Meeting meeting2 = new Meeting();
-		meeting2.setComment("내용내용2");
-		meeting2.setDate(new Date(new GregorianCalendar(2018, 4, 12, 18, 00).getTimeInMillis()));
-		meeting2.setFee(8000);
-		meeting2.setLocation("덕소역");
+		// TODO Auto-generated method stub
 		
-		meetings.add(meeting1);
-		meetings.add(meeting2);
 	}
 
-	
-	
-	private void initMember() {
-		Member member = new Member();
-		member.setName("소문혁");
-		member.setIndex(1);
-		members.add(member);
-		Member member2 = new Member();
-		member2.setName("구명회");
-		member2.setIndex(2);
-		members.add(member2);
-		Member member3 = new Member();
-		member3.setName("이다혜");
-		member3.setIndex(3);
-		members.add(member3);
-	}
-	
-	private void initStudy() {
-		//TODO study바뀌어서 수정해야되긴함.... 혹은 삭제
-		/*StudyListCount study = new StudyListCount();
-		study.setName("자바스터디");
-		study.setGoal("자바마스터");
-		study.setTerm(new Date(new GregorianCalendar(2018, 4, 5).getTimeInMillis()));
+	private void initFeeSpends() {
+		// TODO Auto-generated method stub
 		
-		StudyListCount study2 = new StudyListCount();
-		study2.setName("자바스터디");
-		study2.setGoal("자바마스터");
-		study2.setTerm(new Date(new GregorianCalendar(2018, 4, 5).getTimeInMillis()));
-		
-		StudyListCount study3 = new StudyListCount();
-		study3.setName("자바스터디");
-		study3.setGoal("자바마스터");
-		study3.setTerm(new Date(new GregorianCalendar(2018, 4, 5).getTimeInMillis()));
-		
-		StudyListCount study4 = new StudyListCount();
-		study4.setName("자바스터디");
-		study4.setGoal("자바마스터");
-		study4.setTerm(new Date(new GregorianCalendar(2018, 4, 5).getTimeInMillis()));
-		
-		StudyListCount study5 = new StudyListCount();
-		study5.setName("자바스터디");
-		study5.setGoal("자바마스터");
-		study5.setTerm(new Date(new GregorianCalendar(2018, 4, 5).getTimeInMillis()));
-		
-		StudyListCount study6 = new StudyListCount();
-		study6.setName("자바스터디");
-		study6.setGoal("자바마스터");
-		study6.setTerm(new Date(new GregorianCalendar(2018, 4, 5).getTimeInMillis()));
-		
-		studies.add(study);
-		studies.add(study2);
-		studies.add(study3);
-		studies.add(study4);
-		studies.add(study5);
-		studies.add(study6);*/
-		
-	}
-	
-	public ArrayList<ScheduleBean> getSchedules() {
-		return schedules;
 	}
 
-	public void setSchedules(ArrayList<ScheduleBean> schedules) {
-		this.schedules = schedules;
-	}
-	
-	public ArrayList<Member> getMembers() {
-		return members;
-	}
-	
-	public ArrayList<StudyListCount> getStudies() {
-		return studies;
-	}
-	
-	public void postMember(Member member) {
-		members.add(member);
-	}
-	
-	public void postStudy(StudyListCount study) {
-		studies.add(study);
-	}
-	
-	public ArrayList<Meeting> getMeetings() {
-		return meetings;
+	private void initFeeCollects() {
+		// TODO Auto-generated method stub
+		
 	}
 
-	public void setMeeting(Meeting meeting) {
-		meetings.add(meeting);
+	private void initCategories() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void initBoards() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void initAttends() {
+		// TODO Auto-generated method stub
+		
 	}
 }
