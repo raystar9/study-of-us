e514cac21e5c3ec76b91964ddc18a451417e7a80
Raystar <raystar9@naver.com>
2018-06-03 17:05:06 +0900

    temp

--- src/main/java/servlet/study/each/attendance/AttendanceEach.java
+++ src/main/java/servlet/study/each/attendance/AttendanceEach.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import beans.prototype.Member;
+import beansNew.Member;
 import fakeDB.FakeDB;
 
 /**
