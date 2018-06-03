412cbfa15c94783e2761e6b9f430c2da075268b4
Raystar <raystar9@naver.com>
2018-06-03 17:35:24 +0900

    not clearly ended confirm post page

--- src/main/java/servlet/study/each/attendance/AttendanceEach.java
+++ src/main/java/servlet/study/each/attendance/AttendanceEach.java
@@ -32,7 +32,7 @@ public class AttendanceEach extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		FakeDB db = FakeDB.getInstance();
-		ArrayList<Member> members = db.getMembers();
+		ArrayList<Member> members = db.getStudies().get(0).getMembers();
 		request.setAttribute("members", members);
 		request.getRequestDispatcher("/study/each/attendance/each/record.jsp").forward(request, response);
 	}
