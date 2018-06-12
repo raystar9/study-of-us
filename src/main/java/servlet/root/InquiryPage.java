50e6c036313a69ac63c9f0d9df9a503426de226c
raystar <raystar9@naver.com>
2018-06-12 11:43:33 +0900

    navBar added inquery board

--- src/main/java/servlet/root/InquiryPage.java
+++ src/main/java/servlet/root/InquiryPage.java
@@ -18,7 +18,7 @@ import dao.DatabaseAccounts;
 /**
  * Servlet implementation class InquiryPage
  */
-@WebServlet("/inquiryPage")
+@WebServlet("/inquiry")
 public class InquiryPage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
